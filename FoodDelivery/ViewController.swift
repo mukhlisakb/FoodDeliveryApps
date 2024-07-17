a88590675726ef77200cbf3f41e43a7a64839da2
mukhlisakb <mukhlisakb@gmail.com>
2024-07-17 23:18:29 +0700

    Initial Commit

---
+++ FoodDelivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Mukhlis Akbar on 17/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
